5907d60697b4df5ac7a210ecd5924a26ac6544f0
Bernt Reinhardt <reinhardt@runbox.com>
2020-02-18 14:44:53 +0100

    Added more screens

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const CreateScreen = props => {
+  return (
+    <View>
+      <Text>Create Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CreateScreen;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,7 +4,7 @@ import { Context as BlogContext } from "../context/BlogContext";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 import { Feather } from "@expo/vector-icons";
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
   const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
   return (
     <View>
@@ -14,14 +14,18 @@ const IndexScreen = () => {
         keyExtractor={blogPost => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <View style={styles.row}>
-              <Text style={styles.title}>
-                {item.title} - {item.id}
-              </Text>
-              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                <Feather style={styles.icon} name="trash" />
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Show", { id: item.id })}
+            >
+              <View style={styles.row}>
+                <Text style={styles.title}>
+                  {item.title} - {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            </TouchableOpacity>
           );
         }}
       />
