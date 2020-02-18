343c13f1d80b2fb8cbd328bcd4da66db2e2e9e90
Bernt Reinhardt <reinhardt@runbox.com>
2020-02-18 13:21:50 +0100

    first commit

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,49 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Context as BlogContext } from "../context/BlogContext";
+import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
+import { Feather } from "@expo/vector-icons";
+
+const IndexScreen = () => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
+  return (
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.row}>
+              <Text style={styles.title}>
+                {item.title} - {item.id}
+              </Text>
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name="trash" />
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "gray"
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 24
+  }
+});
+
+export default IndexScreen;
